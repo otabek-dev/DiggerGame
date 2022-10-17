87626144f1f5623bfbbbf234817f53c9a532e32d
Otabek Sh <otabek.pro@hotmail.com>
2022-10-17 23:04:21 +0300

    Created class Player and Terrain

--- DiggerTask.cs
+++ DiggerTask.cs
@@ -7,5 +7,86 @@ using System.Windows.Forms;
 
 namespace Digger
 {
-    
+    public class Terrain : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return true;
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 1;
+        }
+
+        public string GetImageFileName()
+        {
+            return "Terrain.png";
+        }
+    }
+
+    public class Player : ICreature
+    {
+        private int deltaX;
+        private int deltaY;
+
+        protected void UpdateCoordinates()
+        {
+            var creatures = Game.Map[0, 0];
+
+            for (var x = 0; x < Game.MapWidth; x++)
+                for (var y = 0; y < Game.MapHeight; y++)
+                {
+                    creatures = Game.Map[x, y];
+
+                    if (creatures == null) continue;
+
+                    if (creatures is Player)
+                    {
+                        deltaX = x;
+                        deltaY = y;
+                    }
+                }
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            UpdateCoordinates();
+
+            if (Game.KeyPressed == Keys.Left && deltaX > 0)
+                return new CreatureCommand() { DeltaX = -1, DeltaY = 0, TransformTo = null };
+
+            if (Game.KeyPressed == Keys.Right && deltaX < Game.MapWidth - 1)
+                return new CreatureCommand() { DeltaX = 1, DeltaY = 0, TransformTo = null };
+
+            if (Game.KeyPressed == Keys.Up && deltaY > 0)
+                return new CreatureCommand() { DeltaX = 0, DeltaY = -1, TransformTo = null };
+
+            if (Game.KeyPressed == Keys.Down && deltaY < Game.MapHeight - 1)
+                return new CreatureCommand() { DeltaX = 0, DeltaY = 1, TransformTo = null };
+
+
+            return new CreatureCommand();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return false;
+        }
+
+        public int GetDrawingPriority()
+        {
+            return -1;
+        }
+
+        public string GetImageFileName()
+        {
+            return "Digger.png";
+        }
+    }
 }
